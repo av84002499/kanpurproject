947de9ea7cbf8aee252c682349476dc435116409
av7388605503 <av84002499@gmail.com>
2024-05-24 12:37:36 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import Caroimg from "./Caroimg"
+import React from 'react';
+import Caroimg from "./Caroimg";
 
 const Home = () => {
   return (
     <>
-    <div>       
-     <Caroimg />
-    </div>
-    <div className="card" >
-  <img src="..." className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">Card title</h5>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="!" className="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+      <div>       
+        <Caroimg />
+      </div>
+      <div className="card">
+        <div className="row no-gutters">
+          <div className="col-md-4">
+            <img src="2.jpg" className="card-img" alt="..."/>
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <p className="card-text">ISO certification is a seal of approval that a business has met the International Standards 
+              Organization's (ISO) quality process standards. The ISO is a non-governmental organization that sets specifications
+              for products, services, and systems to ensure quality and efficiency. ISO certification demonstrates that an 
+              organization has developed, maintains, and improves its business processes by measuring and comparing performance 
+              to quality standards.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+     
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
