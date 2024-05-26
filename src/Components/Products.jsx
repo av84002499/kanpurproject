9f9f941ee6f837777cb7d12625fd1a0210e22ac9
av7388605503 <av84002499@gmail.com>
2024-05-26 14:34:01 +0530

    update

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -2,60 +2,39 @@ import React from 'react';
 
 const Products = () => {
   return (
-    <div className='container mt-5'>
+    <div className="container">
       <div className="row">
-        <div className="col col-4">
-          {/* Card - 1 */}
-          <div className="card text-bg-primary mb-3" style={{width: '100%', height: '10vh%'}}>
-            <img src="Abcin.png" className="card-img-top" alt="card1" style={{width: '348px', height: '400px'}}/>
-            <div className="card-body">
-              <h5 className="card-title">Name</h5>
-              <h6 className="card-title">₹1,299</h6>
-              <a href="!" className="btn btn-primary">buynow</a>
-            </div>
-          </div>
-        </div>
-
-        <div className="col col-4">
-          {/* Card - 2 */}
-          <div className="card text-bg-primary mb-3" style={{width: '100%', height: '10vh%'}}>
+      
+        <div className="col-md-4">
+          <div className=" card-inverse card-primary text-center">
             <img src="Brenowell.png" className="card-img-top" alt="card1" style={{width: '348px', height: '400px'}}/>
             <div className="card-body">
-            <h5 className="card-title">Name</h5>
-            <h6 className="card-title">₹1,299</h6>
-            <a href="!" className="btn btn-primary">buynow</a>
+              <h4 className="card-title">This is Card #2</h4>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
         </div>
-
-        <div className="col col-4">
-          {/* Card - 3 */}
-          <div className="card text-bg-primary mb-3" style={{width: '100%', height: '10vh%'}}>
+  
+        <div className="col-md-4">
+          <div className=" card-inverse card-success text-center">
             <img src="CARRYPOT.png" className="card-img-top" alt="card1" style={{width: '348px', height: '400px'}}/>
             <div className="card-body">
-            <h5 className="card-title">Name</h5>
-            <h6 className="card-title">₹1,299</h6>
-            <a href="!" className="btn btn-primary">buynow</a>
+              <h4 className="card-title">This is Card #3</h4>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
         </div>
-
-        <div className="col col-4">
-          {/* Card - 4 */}
-          <div className="card text-bg-primary mb-3" style={{width: '100%', height: '10vh%'}}>
+  
+        <div className="col-md-4">
+          <div className="card-inverse card-info text-center">
             <img src="Cylia fort.png" className="card-img-top" alt="card1" style={{width: '348px', height: '400px'}}/>
             <div className="card-body">
-            <h5 className="card-title">Name</h5>
-            <h6 className="card-title">₹1,299</h6>
-            <a href="!" className="btn btn-primary">buynow</a>
+              <h4 className="card-title">This is Card #4</h4>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
           </div>
         </div>
-
-        <div className="col col-4">
-          {/* Card - 5 */}
-         
-        </div>
+  
       </div>
     </div>
   );
